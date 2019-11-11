db62cd5b647479d829de8c674ac0d03d8e2f553b
LuluFT <l.weiselowski@gmail.com>
2019-11-11 22:40:34 +0100

    xxx

--- Kopfrechner.py
+++ Kopfrechner.py
@@ -1,40 +1,50 @@
 import random #das Modul Random wird importiert. Dort ist der Befehl "shuffle" vorhanden um eine Liste durchzuwürfeln.
+import time # hier wird das Modul Time importiert um die Möglichkeit zu haben, mit der Zeit zu arbeiten.
 
-#Variabel, wie viele Versuche man hat bis man verloren hat
-leben = 3
+leben = 3#Variabel, wie viele Versuche man hat bis man verloren hat
 
+lstEins = [] #Leere Liste wird erstellt 
 
-#Leere Liste wird erstellt 
-lstEins = []
 #Liste wird gefüllt mit den Werten 0-100
 for i in range (0,100):
     lstEins.append(i)
 
 #Liste wird einmal durchgewürfelt, sodass alle Inhalte an einer anderen Position stehen
 random.shuffle(lstEins)
-#Lösung ist gleich die Addition von der 3ten und 60ten Stelle aus der Liste
 
+#Lösung ist gleich die Addition von der 3ten und 60ten Stelle aus der Liste, welche oben erstellt wurde.
 
-Aufgabe = 1 #Nr der Aufgabe
 lösung = (lstEins[3]+lstEins[60])
-#Solange das Leben nicht unter 0 fällt, wiederholen sich die Aufgaben
+Aufgabe = 1 #Variabel um die aktuelle Nummer der Aufgabe anzuzeigen.
+
+#Hier werden mehrere Absätze erstellt, welche im Terminal angezeigt werden.
+print("")
+print("")
+print("")
+print("")
+
+#Hier wird die Aufgabe initialisiert. Es wird ein String angezeigt mit unten stehendem Text und anschließend wird die Aufgaben NR und die Aufgabe angezeigt.
 print ("Hallo, bitte geben Sie die Lösung ein, zu der Aufgabe:")
 print("")
 print ("Aufgabe Nr:",Aufgabe)
 print (lstEins[3],"+",lstEins[60],"=",)
 
+ts = time.time()#Startzeit wird gestartet
 
+    
 
-while leben != 0:  
+while leben != 0:  #Während das Leben ungleich 0 ist, werden folgende Sachen ausgeführt.
     
     
-    lösung = (lstEins[3]+lstEins[60])
+    lösung = (lstEins[3]+lstEins[60]) # Variabel Lösung wird deklariert.
     
-    #die Konsole verlangt einen Input als Int
-    input1 = int(input())
+   
+    input1 = int(input())#Variabel Input1 wird deklariert. Hier wird eine Ganzzahl als Input verlangt.
+
     
-    #wenn der Input den gleichen Wert wie die Lösung hat, wird eine neue Aufgabe gestellt.
-    if input1 == lösung:
+    #Wenn der Input gleich der Lösung ist, wird zur Aufgaben Zahl eine 1 addiert.
+    #Die Liste wird noch einmal durchgewürfelt und es wird eine neue Aufgabe gestellt.
+    if input1 == lösung: 
        
         random.shuffle(lstEins)
         Aufgabe = (Aufgabe+1)
@@ -45,9 +55,9 @@ while leben != 0:
         print("Nächste Aufgabe!")
         print ("Aufgabe Nr:",Aufgabe)
         print (lstEins[3],"+",lstEins[60],"=",)
-    #wenn der Input ungleich der Lösung ist, wird ein Leben abgezogen und die Aufgabe wiederholt.        
-    else:
         
+    #wenn der Input ungleich der Lösung ist, wird ein Leben abgezogen und die Aufgabe wiederholt.   
+    else:       
         print ("Schade, leider falsch")
         leben = (leben -1 )
         print ("Leben übrig:",leben) 
@@ -56,10 +66,30 @@ while leben != 0:
         print("Neuer Versuch:")
         print("")
         print (lstEins[3],"+",lstEins[60],"=",)
-    #wenn man kein Leben mehr hat, wird einem unter die Nase gerieben, dass man verloren hat und das Programm beendet sich.
-    if leben == 0:
-        print ("Leider verloren!")
-    
 
 
+
+#wenn man kein Leben mehr hat, wird einem unter die Nase gerieben, dass man verloren hat und das Programm beendet sich.
+if leben == 0:
+    print("Der Highscore beträgt: ", Aufgabe)
+    print ("Leider verloren!")
     
+    #Hier wird die Startzeit von der jetztigen Zeit abgezogen. Anschließend wird einem die gesamte Dauer des Programms angezeigt
+    #Hier wird unterscheiden, ob es eine Sekunde, ein paar Sekunden oder eine Minute lief.
+    td =(time.time() - ts)
+    Sekunde = td.__round__(2)
+    
+    if Sekunde == 1:
+        print("Das Programm lief:",Sekunde.__round__(),"Sekunde")
+    elif Sekunde <= 60:
+        print("Das Programm lief:",Sekunde.__round__(),"Sekunden")
+    elif Sekunde >= 60:
+        Minute = int(Sekunde/60)
+        if Minute == 1:
+            print("Das Programm lief:",Minute,"Minute")
+        elif Minute >= 1:
+            print ("Das Programm lief:",Minute,"Minuten")
+    exit()
+       
+
+ 
--- Python/Test_Hello_World.py
+++ Python/Test_Hello_World.py
@@ -1,10 +1,5 @@
+import time
 import random 
 
-lop = 4
 
-for lol in range (9):
-    if lop == 2:
-        print ("hiiiii")
-    else:
-        print ("was geht ab du hundesohn ")
-    
+ts = time.sleep(2)
